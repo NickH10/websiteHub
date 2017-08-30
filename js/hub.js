6c423cbca5660222f30471fdf094b61f83f77fee
Nicholas Hughes <nhughes@nolo.com>
2017-08-30 09:06:57 -0700

    added in parallax effect to background, cleaned some javascript and html jankiness

--- js/hub.js
+++ js/hub.js
@@ -1,5 +1,30 @@
-function myFunction(element) {
-    element.classList.toggle("toggled");
-    // console.log(element.classList);
-    // window.alert("hamburgered");
+$(document).ready(function(){
+
+	backgroundParallax();
+
+});
+
+// change background size on window resize
+// $(window).resize(function() {
+// 	$('html').css({
+// 		width: $(window).outerWidth(),
+// 		height: $(window).outerHeight()
+// 	})
+// })
+
+// toggles whether hamburger is on or off
+function hamburgerToggle(element) {
+    element.classList.toggle('toggled');
+}
+
+// creates background parallax effect
+var backgroundParallax = function() {
+	$('#container').mousemove(function( event ) {
+		var containerWidth = $(this).innerWidth(),
+		    containerHeight = $(this).innerHeight(),
+		    mousePositionX = ((event.pageX / containerWidth) * 100)*.05,
+		    mousePositionY = ((event.pageY /containerHeight) * 100)*.05;
+
+		$(this).css('background-position', mousePositionX + '%' + ' ' + mousePositionY + '%');
+	});
 }
